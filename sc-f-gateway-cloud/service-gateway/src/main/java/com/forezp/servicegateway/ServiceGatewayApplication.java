11e67ce34c576a15a9e6857eab9e1b13c125ee33
yoli <yoli@tripadvisor.com>
2019-11-19 16:25:25 +0800

    to nacos

--- sc-f-gateway-cloud/service-gateway/src/main/java/com/forezp/servicegateway/ServiceGatewayApplication.java
+++ sc-f-gateway-cloud/service-gateway/src/main/java/com/forezp/servicegateway/ServiceGatewayApplication.java
@@ -2,11 +2,13 @@ package com.forezp.servicegateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-@EnableEurekaClient
+@EnableDiscoveryClient
 public class ServiceGatewayApplication {
 
     public static void main(String[] args) {
@@ -16,5 +18,19 @@ public class ServiceGatewayApplication {
     @Bean
     public RequestTimeGatewayFilterFactory requestTimeGatewayFilterFactory() {
         return new RequestTimeGatewayFilterFactory();
+    }
+
+    @Bean
+    public RouteLocator customerRouteLocator(RouteLocatorBuilder builder) {
+        // @formatter:off
+        return builder.routes()
+                .route(r -> r.path("/login/**")
+                        .filters(f -> f.filter(new BlacklistUserFilter()))
+                        .uri("http://localhost:8761/hi")
+                        .order(0)
+                        .id("verify_router")
+                )
+                .build();
+        // @formatter:on
     }
 }
--- sc-f-gateway-cloud/service-hi/src/main/java/com/forezp/servicehi/ServiceHiApplication.java
+++ sc-f-gateway-cloud/service-hi/src/main/java/com/forezp/servicehi/ServiceHiApplication.java
@@ -3,14 +3,14 @@ package com.forezp.servicehi;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@EnableEurekaClient
+@EnableDiscoveryClient
 @RestController
 public class ServiceHiApplication {
 
@@ -27,10 +27,10 @@ public class ServiceHiApplication {
     }
 
 
-//    @RequestMapping("/demo/hi")
-//    public String qsw(@RequestParam(value = "name", defaultValue = "forezp") String name) {
-//        return "hi " + name + " ,i am from port:" + port;
-//    }
+    @RequestMapping("/demo/hi")
+    public String sayhi(@RequestParam(value = "name", defaultValue = "forezp") String name) {
+        return "demo " + name + " ,i am from port:" + port;
+    }
 }
 
 
